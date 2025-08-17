7e6c7991436eeb70e8d04094c3d215ee6ff64b0e
achala2702 <achala2702@gmail.com>
2025-08-17 17:53:22 +0530

    notification service initialized

---
+++ notification-server/src/test/java/com/achala/notification_server/NotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achala.notification_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
